97345aea60496b91157e8cebdec014d40a552916
geka <geka.kud@gmail.com>
2021-11-12 11:11:31 +0200

    Update api.js

--- BikeRouteService/wwwroot/src/api/api.js
+++ BikeRouteService/wwwroot/src/api/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const apiUrlDev = "http://localhost:6001";
 const apiUrlProd = "https://bikeroutewin.azurewebsites.net";
-export const routesApi = `${apiUrlProd}/api/Routes/`;
+const apiUrlProdReact = "https://reactjsbikeroutes.azurewebsites.net";
+export const routesApi = `${apiUrlProdReact}/api/Routes/`;
 
 const instance = axios.create({
   baseURL: routesApi,
